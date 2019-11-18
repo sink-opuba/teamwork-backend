bd292a3e17067960433a3d10d4a22c1b4953b98a
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-11-18 19:27:08 +0100

    configure cloudinary and coveralls

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1/auth', userRoutes);
 module.exports = app;
