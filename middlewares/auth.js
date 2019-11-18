1ddc0600101bf4bbd75b0b4bf97fe2150bfca160
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-11-18 20:06:33 +0100

    implement user check auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -30,19 +30,31 @@ exports.checkIfAdmin = async (req, res, next) => {
   }
 };
 
-// module.exports = (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization.split(' ')[1];
-//     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-//     const userId = decodedToken.userId;
-//     if (req.body.userId && req.body.userId !== userId) {
-//       throw 'Invalid User ID';
-//     } else {
-//       next();
-//     }
-//   } catch (error) {
-//     res.status(401).json({
-//       error: new Error('Invalid request')
-//     });
-//   }
-// };
+exports.checkIfUser = async (req, res, next) => {
+  try {
+    let token = req.headers.authorization;
+    if (!token) {
+      throw 'You need an authentication token to access this resource.';
+    }
+    [, token] = token.split(' ');
+    const decodedToken = jwt.verify(
+      token,
+      'I AM STILL Thinking of a token secret, wait for it! ..lol'
+    );
+    const { userId, email } = decodedToken;
+    const result = await db.query(
+      `SELECT * from users where userId = $1 AND email = $2`,
+      [userId, email]
+    );
+
+    if (!result.rows[0]) {
+      throw 'Invalid Request! Your need to signup inorder to make this request.';
+    }
+    next();
+  } catch (error) {
+    res.status(401).json({
+      status: 'error',
+      error
+    });
+  }
+};
