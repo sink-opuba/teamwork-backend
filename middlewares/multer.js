8ce5bf89d1bccaa2f2733dc39412004e8aa20992
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2019-11-18 20:22:35 +0100

    configure multer middleware

---
+++ middlewares/multer.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    callback(null, name + '_' + Date.now());
+  }
+});
+
+const fileFilter = (req, file, callback) => {
+  if (
+    file.mimetype === 'image/gif' ||
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png'
+  ) {
+    callback(null, true);
+  } else {
+    // reject file that is not gif, jpeg or png
+    callback({ message: 'Unsupported file format' }, false);
+  }
+};
+
+module.exports = multer({ storage, fileFilter }).single('image');
+// 'image' is the fieldname -- the name attribute on the form data
